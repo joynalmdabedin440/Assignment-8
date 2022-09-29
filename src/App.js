bf60e983aa818a07c7e5c7afdf97e212c54ee2cb
joynalmdabedin <joynalmdabedin440@gmail.com>
2022-09-29 11:20:23 +0600

    fake data add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <p>its done</p>
+      <button className='btn btn-danger'>hello js</button>
     </div>
   );
 }
